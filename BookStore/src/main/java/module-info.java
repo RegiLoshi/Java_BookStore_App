021eb711de8f502e0c689504e1c0969c30ce67c6
Alvin <kollcakualvin@gmail.com>
2024-01-05 20:30:16 +0100

    Version 1

---
+++ BookStore/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module application.bookstore {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens application.bookstore to java.base;
+    exports application.bookstore;
+}
