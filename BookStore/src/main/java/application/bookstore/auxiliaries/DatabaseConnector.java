1c7e51d681377791b903339a823312b80bf99903
RegiLoshi <rloshi111@gmail.com>
2024-01-08 00:58:32 +0100

    ProfileView added with edit function independent of role, class with static methods for editing users details added , also some exceptions in case a unique value is added when another one already exists with same values

--- BookStore/src/main/java/application/bookstore/auxiliaries/DatabaseConnector.java
+++ BookStore/src/main/java/application/bookstore/auxiliaries/DatabaseConnector.java
@@ -2,16 +2,14 @@ package application.bookstore.auxiliaries;
 
 
 public interface DatabaseConnector {
-
+     /*
      String JDBC_URL = "jdbc:mysql://localhost/bookstore";
      String USER = "root";
      String PASSWORD = "workbench12$";
-
-     /*
+*/
      String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/epokaproject";
      String USER = "root";
       String PASSWORD = "11112003";
-      */
 
 }
 
