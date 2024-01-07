f2a65b22e3409626253132a7ef9d6a309fd0030a
Alvin <kollcakualvin@gmail.com>
2024-01-07 08:22:28 +0100

    Small change

--- BookStore/src/main/java/application/bookstore/auxiliaries/DatabaseConnector.java
+++ BookStore/src/main/java/application/bookstore/auxiliaries/DatabaseConnector.java
@@ -4,7 +4,7 @@ package application.bookstore.auxiliaries;
 public interface DatabaseConnector {
      static final String JDBC_URL = "jdbc:mysql://localhost/bookstore";
      static final String USER = "root";
-     static final String PASSWORD = "Bp208$squirky";
+     static final String PASSWORD = "workbench12$";
 
 }
 
